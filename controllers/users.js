0177e01f049c5758c76735f71b7f6057fd8db34a
Alex Kim <alexander.e.kim@gmail.com>
2016-04-18 12:20:39 -0500

    updated controller

--- controllers/users.js
+++ controllers/users.js
@@ -1,9 +1,24 @@
 var express = require('express');
 var controller = express.Router();
+var UserAccount = require('../models/UserAccount');
 
 /* GET users listing. */
 controller.get('/', function(req, res, next) {
   res.send('respond with a resource');
 });
+
+controller.get('/logout', function(req, res, next){
+  res.json({ 'message': 'logout not yet implemented'});
+})
+
+controller.post('/register', function(req, res, next){
+  res.json({ 'message': 'register not yet implemented'});
+
+})
+
+controller.post('/login', function(req, res, next){
+  res.json({ 'message': 'login not yet implemented'});
+})
+
 
 module.exports = controller;
